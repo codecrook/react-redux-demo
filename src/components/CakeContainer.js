de42f6558ea56eb49d11b764c1b7560cc6bb64ac
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-07-09 23:46:09 +0530

    ADD: useSelector, useDispatch hooks from react-redux

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { buyCake } from '../redux';
 
-function CakeContainer({ numOfCakes, buyCake }) {
+function CakeContainer() {
+    const { numOfCakes } = useSelector(state => ({ numOfCakes: state.numOfCakes }));
+    const dispatch = useDispatch();
     return (
         <div>
             <h2>No. of cakes: {numOfCakes}</h2>
-            <button onClick={buyCake}>Buy Cake</button>
+            {/* <button onClick={buyCake}>Buy Cake</button> */}
+            <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
         </div>
     );
 }
 
-const mapStateToProps = state => ({ numOfCakes: state.numOfCakes });
-const mapDispatchToProps = dispatch => ({ buyCake: () => dispatch(buyCake()) });
+// const mapStateToProps = state => ({ numOfCakes: state.numOfCakes });
+// const mapDispatchToProps = dispatch => ({ buyCake: () => dispatch(buyCake()) });
 
-export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
+// export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
+
+export default CakeContainer;
