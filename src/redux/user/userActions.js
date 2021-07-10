d94c14cad9f58a318e1ae5885be758eb8337e537
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-07-11 00:36:40 +0530

    ADD: async action fetchUsers to fetch users data

--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -1,5 +1,16 @@
+import axios from 'axios';
 import { FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS, FETCH_USERS_FAILURE } from './userTypes';
 
 export const fetchUsersRequest = () => ({ type: FETCH_USERS_REQUEST });
 export const fetchUsersSuccess = users => ({ type: FETCH_USERS_SUCCESS, payload: users });
 export const fetchUsersFailure = error => ({ type: FETCH_USERS_FAILURE, payload: error });
+
+export const fetchUsers = () => dispatch => {
+    dispatch(fetchUsersRequest());
+    axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(response => {
+            const { data: users } = response;
+            dispatch(fetchUsersSuccess(users));
+        })
+        .catch(error => dispatch(fetchUsersFailure(error.message)));
+};
